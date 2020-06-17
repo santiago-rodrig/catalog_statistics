6e351f1c9d7003970f0e0cbd1cc8465d47d58955
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-17 12:01:37 -0400

    feature: fetch companies action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // src/actions/index.js
+import 'node-fetch';
+const fetchCompanies = async () => {
+  const jsonResponse = await fetch(`https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=dcc4a7c2f57cf7b1f8804163eca5017b`, { headers: { 'Content-Type': 'application/json' } });
+  const data = await jsonResponse.json();
+  return data;
+}
+export default fetchCompanies;
