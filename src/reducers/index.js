7c1bdf629818c93d3a9656753ff17a72687d930d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-17 11:51:30 -0400

    feature: companies reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,11 @@
 // src/reducers/index.js
+const FETCH_COMPANIES = 'FETCH_COMPANIES';
+const reducer = (state={}, action) => {
+  switch (action.type) {
+    case FETCH_COMPANIES:
+      return action.companies
+    default:
+      return state
+  }
+};
+export default reducer;
