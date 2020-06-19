68df5195900cd3e2d96f020f8203fd982c6dd720
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-19 10:52:03 -0400

    fix: just return arrow fn

--- src/components/CompaniesList.js
+++ src/components/CompaniesList.js
@@ -3,22 +3,20 @@ import PropTypes from 'prop-types';
 import CompanyItem from './CompanyItem';
 import styles from './CompaniesList.module.css';
 
-const Component = ({ companies }) => {
-  return (
-    <>
-      <h2
-        style={{
-          maxWidth: '80%', textAlign: 'center', margin: '0 auto', marginTop: '3rem',
-        }}
-      >
-        { companies.length === 0 ? 'Search something' : 'Search results' }
-      </h2>
-      <ul className={styles.list}>
-        {companies.map(c => <CompanyItem key={c.symbol} company={c} />)}
-      </ul>
-    </>
-  );
-};
+const Component = ({ companies }) => (
+  <>
+    <h2
+      style={{
+        maxWidth: '80%', textAlign: 'center', margin: '0 auto', marginTop: '3rem',
+      }}
+    >
+      { companies.length === 0 ? 'Search something' : 'Search results' }
+    </h2>
+    <ul className={styles.list}>
+      {companies.map(c => <CompanyItem key={c.symbol} company={c} />)}
+    </ul>
+  </>
+);
 
 Component.propTypes = {
   companies: PropTypes.arrayOf(PropTypes.object).isRequired,
