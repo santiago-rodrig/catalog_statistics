aff50fabed11bf19c5963994512d68a689d9879c
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-22 11:27:56 -0400

    feature: some tests

---
+++ src/containers/Companies.test.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import App from '../components/App';
+
+test('it renders a form', () => {
+  const { getByRole } = render(<App />);
+  expect(getByRole('form')).toBeInTheDocument();
+});
+
+test('it renders a textbox', () => {
+  const { getByRole } = render(<App />);
+  expect(getByRole('textbox')).toBeInTheDocument();
+});
